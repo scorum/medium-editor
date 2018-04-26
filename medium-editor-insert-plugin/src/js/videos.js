c61984d6ac5f4c5f7a8e226db76b0e257c99fa96
jekahm <jekaheavysmoke@gmail.com>
2018-04-26 18:36:43 +0300

    Fixed Videos addon functionality.

--- medium-editor-insert-plugin/src/js/videos.js
+++ medium-editor-insert-plugin/src/js/videos.js
@@ -15,14 +15,63 @@
      * Videos object
      * @inheritDoc
      */
+    function Videos (el, options) {
+        this.el = el;
+        this.$el = $(el);
+        this.templates = window.MediumInsert.Templates;
+        this.core = this.$el.data('plugin_'+ pluginName);
 
-    function Videos(el, options) {
-        commonEmbedsAddon.apply(this, arguments);
+        this.options = $.extend(true, {}, options);
+
+        this._name = pluginName;
+
+        this.init();
     }
 
-    Videos.prototype = Object.create(commonEmbedsAddon.prototype);
-    Videos.prototype.constructor = Videos;
+    /**
+     * Initialization
+     *
+     * @return {void}
+     */
 
+    Videos.prototype.init = function () {
+        this.events();
+    };
+
+    /**
+     * Event listeners
+     *
+     * @return {void}
+     */
+
+    Videos.prototype.events = function () {
+
+    };
+
+    /**
+     * Get the Core object
+     *
+     * @return {object} Core object
+     */
+    Videos.prototype.getCore = function () {
+        return this.core;
+    };
+
+    /**
+     * Add custom content
+     *
+     * This function is called when user click on the addon's icon
+     *
+     * @return {void}
+     */
+
+    Videos.prototype.add = function () {
+        commonEmbedsAddon.prototype.add.apply(this, arguments);
+    };
+
+    Videos.prototype.togglePlaceholder = function () {
+        commonEmbedsAddon.prototype.togglePlaceholder.apply(this, arguments);
+    };
 
     /** Plugin initialization */
 
