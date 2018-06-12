a3a81010ab7a1014167294879a2f2320bcd81b65
jekahm <jekaheavysmoke@gmail.com>
2018-06-12 13:38:16 +0300

    Coachmarks funcionality.

--- medium-editor-insert-plugin/src/js/core.js
+++ medium-editor-insert-plugin/src/js/core.js
@@ -8,6 +8,23 @@
             editor: null,
             enabled: true,
             showButtonLabel: '<span>+</span>',
+            helpCoachmarksOptions: {
+                elements: {
+                    showButtonElement: {
+                        id: null,
+                        title: null,
+                        content: null,
+                        buttonText: null,
+                    },
+                    editorElement: {
+                        id: null,
+                        title: null,
+                        content: null,
+                        buttonText: null,
+                    },
+                },
+                helpers: null,
+            },
             addons: {
                 images: true, // boolean or object containing configuration
                 videos: true,
@@ -123,7 +140,8 @@
                 $.proxy(that, 'removeCaptionPlaceholder')($(e.target));
             })
             .on('mouseenter', '.medium-insert-action', $.proxy(this, 'hoverInInsertActionButton'))
-            .on('mouseleave', '.medium-insert-action', $.proxy(this, 'hoverOutInsertActionButton'));
+            .on('mouseleave', '.medium-insert-action', $.proxy(this, 'hoverOutInsertActionButton'))
+            .on('input', $.proxy(this, 'checkEditorToolbarCoachmark'));
 
         $(window).on('resize', $.proxy(this, 'positionButtons', null));
     };
@@ -426,6 +444,10 @@
                 if ($p.closest('blockquote').length === 0) {
                     $p.addClass('medium-insert-active');
 
+                    // Processes Coachmark functionality
+                    var coachmarkElementData = this.options.helpCoachmarksOptions.elements.showButtonElement;
+                    this.addHelpCoachmark(coachmarkElementData, this.$el.find('.medium-insert-buttons'));
+
                     if (activeAddon === 'images') {
                         this.$el.find('.medium-insert-buttons').attr('data-active-addon', activeAddon);
                     } else {
@@ -484,6 +506,10 @@
             if (hasPlaceholder) {
                 this.$el.focus();
             }
+
+            var coachmarkId = this.options.helpCoachmarksOptions.elements.showButtonElement.id;
+            var $currentCoachmarkElement = $el.find(`.medium-insert-buttons .medium-insert-help-coachmark[data-id="${coachmarkId}"]`);
+            this.removeHelpCoachmark(coachmarkId, $currentCoachmarkElement);
         }
     };
 
@@ -708,7 +734,7 @@
      * Shows Action button tooltip
      * @param e
      */
-    Core.prototype.hoverInInsertActionButton = function(e) {
+    Core.prototype.hoverInInsertActionButton = function (e) {
         var $el = $(e.target).closest('.medium-insert-action');
         var tooltipClass = 'medium-insert-action-tooltip';
 
@@ -721,7 +747,7 @@
      * Hides Action button tooltip
      * @param e
      */
-    Core.prototype.hoverOutInsertActionButton = function(e) {
+    Core.prototype.hoverOutInsertActionButton = function (e) {
         var $el = $(e.target).closest('.medium-insert-action');
         var tooltipClass = 'medium-insert-action-tooltip';
 
@@ -729,6 +755,89 @@
             $el.find(`.${tooltipClass}`).remove();
         }
     };
+
+    /**
+     * Appends Coachmark to certain element
+     * @param coachmarkElementData
+     * @param $wrapper
+     * @returns {boolean}
+     */
+    Core.prototype.addHelpCoachmark = function (coachmarkElementData, $wrapper) {
+        var that = this;
+        var coachmarksOptions = this.options.helpCoachmarksOptions;
+        var coachmarkHelpers = coachmarksOptions.helpers;
+        var coachmarkId = coachmarkElementData.id;
+
+        if ($wrapper.length === 0 || !coachmarkHelpers) {
+            return false;
+        }
+
+        var currentCoachmarkElementSelector = `.medium-insert-help-coachmark[data-id="${coachmarkId}"]`;
+
+        // If coachmark id for `show button` doesn't exist in local store then adds it
+        if (!coachmarkHelpers.checkOnExistence(coachmarkId) && $wrapper.find(currentCoachmarkElementSelector).length === 0) {
+            $wrapper.append(this.templates['src/js/templates/coachmark-block.hbs']({
+                id: coachmarkId,
+                title: coachmarkElementData.title,
+                content: coachmarkElementData.content,
+                buttonText: coachmarkElementData.buttonText,
+            }));
+
+            console.log('===== addCoachmark', coachmarkId);
+
+            $wrapper.find(currentCoachmarkElementSelector).fadeIn(500);
+
+            $wrapper.on('click', `.medium-insert-help-coachmark[data-id="${coachmarkId}"] button`, function () {
+                that.removeHelpCoachmark(coachmarkId, $wrapper.find(currentCoachmarkElementSelector));
+            });
+        }
+    };
+
+    /**
+     * Removes Coachmark from certain element (+ adds its ID to local store)
+     * @param $wrapper
+     */
+    Core.prototype.removeHelpCoachmark = function (id, $currentCoachmarkElement) {
+        var coachmarksOptions = this.options.helpCoachmarksOptions;
+        var coachmarkHelpers = coachmarksOptions.helpers;
+
+        if ($currentCoachmarkElement.length !== 0) {
+            console.log('===== beforeRemove');
+
+            $currentCoachmarkElement.fadeOut(500, function () {
+                $(this).remove();
+
+                console.log('===== remove from DOM', id); /////
+
+                if (!coachmarkHelpers.checkOnExistence(id)) {
+                    console.log('===== remove from Local store', id); /////
+                    coachmarkHelpers.addCoachMarkId(id);
+                }
+            });
+        }
+    };
+
+    /**
+     * Checks whether to add or remove Coachmark to Editor's content element
+     * @param e
+     */
+    Core.prototype.checkEditorToolbarCoachmark = function(e) {
+        if (this.$el) {
+            var editorPureText = $(e.target).find('p, h2, h3, blockquote').not($('.medium-insert-embeds > p')).text().trim();
+            var coachmarkElementData = this.options.helpCoachmarksOptions.elements.editorElement;
+            var coachmarkId = coachmarkElementData.id;
+            var currentCoachmarkElementSelector = `.medium-insert-help-coachmark[data-id="${coachmarkId}"]`;
+
+            console.log(editorPureText.length);
+
+            // Processes Coachmark functionality
+            if (editorPureText.length > 20 && editorPureText.length < 30) {
+                this.addHelpCoachmark(coachmarkElementData, this.$el);
+            } else {
+                this.removeHelpCoachmark(coachmarkId, this.$el.find(currentCoachmarkElementSelector));
+            }
+        }
+    }
 
     /**
      * Async delay helper
